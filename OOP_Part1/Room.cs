a285f8e4e108bc55205bc711516609e73f9e8a57
Mohammed Al Abri <striker.m901@gmail.com>
2026-06-16 09:12:37 +0400

    Add Room class with properties and display method
    Introduced Room.cs containing the Room class and RoomType enum in the OOP_Part1 namespace. The class manages room details, availability, and provides a method to display room information.

---
+++ OOP_Part1/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Part1
+{
+    public enum RoomType { Single, Double, Suite }
+    internal class Room
+    {
+        public string roomNumber { get; private set; }
+        public RoomType roomType { get; private set; }
+        public double pricePerNight { get; private set; }
+        public bool   isAvailable { get; set; }
+
+        public Room(string roomNumber, RoomType roomType, double pricePerNight)
+        {
+            this.roomNumber = roomNumber;
+            this.roomType = roomType;
+            this.pricePerNight = pricePerNight;
+            this.isAvailable = true; // always true on creation;
+        }
+
+        public void displayRoom() 
+        {
+            Console.WriteLine($"\nRoom: {roomNumber}  |   Type: {roomType}    |   Price: {pricePerNight:F2} OMR   |   Available: {isAvailable}");
+        }
+    }
+}
